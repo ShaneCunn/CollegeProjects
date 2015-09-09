628aa6f8b24c6bcc639d02f2eca11bd65df4dbcf
Shane Cunningham <mayhem2277@gmail.com>
2015-09-09 18:04:26 +0100

    example of using Scanner

---
+++ CollegeProjects/src/SystemIn.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class SystemIn {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Enter Name here:"); //display string
+		
+		String newname; //declares new string variable
+		
+		Scanner ScanIn = new Scanner(System.in); //create a 
+		newname = ScanIn.nextLine();
+		ScanIn.close();
+		System.out.println("Hello " + newname); //adds together newstring and string variable
+				
+				
+	}
+
+}
