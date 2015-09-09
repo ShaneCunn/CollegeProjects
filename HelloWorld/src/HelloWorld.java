ac0fcc573d96ae1fdb5bfb250b68eb6a5bbc8948
Shane Cunningham <mayhem2277@gmail.com>
2015-09-09 16:40:06 +0100

    Basic Hello world java program

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// main method that execution of this application
+		System.out.println("Hello Shane"); //display string
+	}
+
+}
