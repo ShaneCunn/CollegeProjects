8d8aa853ad39b104ab518b7bba11ef8b83727f3f
Shane Cunningham <mayhem2277@gmail.com>
2015-09-14 19:46:15 +0100

    simple people counter class

---
+++ CollegeProjects/src/interger.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner; //import scanner that read in system in
+public class interger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner single = new Scanner(System.in);
+		
+		int lads, girls, people;
+			lads = 6;
+			girls= 3;
+			people = lads + girls;
+				System.out.println("The is Total: " + people);
+
+	}
+
+}
