5da8b17e96a1e7433e1fe22f296582451350ef67
Shane Cunningham <mayhem2277@gmail.com>
2015-09-14 19:24:23 +0100

    input scanner class

---
+++ CollegeProjects/src/userinput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; //import scanner that scans input		
+public class userinput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Store = new Scanner(System.in); // new variable that store data from the scanner in
+		System.out.println(Store.nextLine()); // output the store variable to the screen
+	}
+
+}
