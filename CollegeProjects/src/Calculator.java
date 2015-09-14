57039951727cb14440309b51f9666351265be1d1
Shane Cunningham <mayhem2277@gmail.com>
2015-09-14 19:36:07 +0100

    Basic calculator class

---
+++ CollegeProjects/src/Calculator.java
@@ -0,0 +1,18 @@
+import  java.util.Scanner; //import scanner that read in system in
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner Cal = new Scanner(System.in);
+		
+		double fnum, snum, answer; // 3 double varible
+		
+		System.out.println("Enter the first number:");
+		fnum = Cal.nextDouble(); //stores first number			
+		System.out.println("Enter the second number:");
+		snum = Cal.nextDouble(); // store the second number		
+		answer = fnum + snum; // add toget the first and second numbers
+		System.out.println(answer); // outputs the answer variable to screen
+			
+		
+	}
+}
