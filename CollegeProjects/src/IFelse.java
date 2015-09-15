fa50920851b6d2566f92c599b44d7921acd745c3
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 13:23:38 +0100

    ifelse statement

---
+++ CollegeProjects/src/IFelse.java
@@ -0,0 +1,27 @@
+
+public class IFelse {
+	
+	
+	/*
+	 * == is equal to
+	 * != is not equal to
+	 * > is greater than 
+	 * < is less than 
+	 * >= is greater than or equal to
+	 * <= is less than or equal to
+	*/
+
+	public static void main(String[] args) {
+	
+		
+		int x = 10; // stores a int with a value of 10
+		
+		if (x== 10) {
+			System.out.println("yes x == 10"); // if the number = 10 , it will print out yes/true statement
+		}
+		else {
+			System.out.println("no x != 10"); // if the number is not = to , it print out no
+		}
+	}
+
+}
