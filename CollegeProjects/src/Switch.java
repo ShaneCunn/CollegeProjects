0b1b3a2897651ded90d9ca5aeb199b3d078a472d
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 14:22:47 +0100

    this a set of variable to be called once.

--- CollegeProjects/src/Switch.java
+++ CollegeProjects/src/Switch.java
@@ -13,20 +13,23 @@ public class Switch {
 	 */
 	public static void main(String[] args) {
 		// can be a byte , short, int , or char
-		int score = 90;
+		int score = 95;
 		switch (score )
 		{
+		case 100 :
+		case  95 :
 		case 90 : 
 			System.out.println("Very Good");
 			break; // break out of the switch statement
 		case 60 :
-			System.out.println("Good");
-		
+			System.out.println("Good");		
 			break; // break out of the switch statement 
 		case 30 :
 			System.out.println("not Good");
 			break; // break out of the switch statement
-			
+		default :
+			System.out.println("the grade is not defined");
+			break; // break out of the switch statement
 			
 			
 		}
