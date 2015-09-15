994aba357f5fb66627a5ac2f0fa70be7f38b2fec
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 14:32:07 +0100

    while loops

---
+++ CollegeProjects/src/While.java
@@ -0,0 +1,17 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int  a = 0;
+		
+		while (a < 10) // while a is less than one is loops through
+		{
+			System.out.println(a);
+			a++; // adds +1 to the A counter
+		}
+
+	}
+
+}
