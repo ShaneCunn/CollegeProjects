b569ee374451a4030a08081410465f037001167c
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 14:56:18 +0100

    reverses the loop

--- CollegeProjects/src/While.java
+++ CollegeProjects/src/While.java
@@ -4,12 +4,12 @@ public class While {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int  a = 0;
+		int  a = 10;
 		
-		while (a < 10) // while a is less than one is loops through
+		while (a >= 1) // while a is less than one is loops through
 		{
 			System.out.println(a);
-			a++; // adds +1 to the A counter
+			a--; // adds +1 to the A counter
 		}
 
 	}
