76613c192e6ac998b2f4e730dd426b4e0c6aa08c
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 13:36:37 +0100

    OR operator

--- CollegeProjects/src/LogicalOps.java
+++ CollegeProjects/src/LogicalOps.java
@@ -3,11 +3,11 @@ public class LogicalOps {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int subject1 = 40;
+		int subject1 = 30;
 		int subject2 = 60;
 		// 	&& AND operator    
 		//  || OR operator
-		if ((subject1 >= 35) && (subject2 >= 35)) {
+		if ((subject1 >= 35) || (subject2 >= 35)) {
 			System.out.println("the condition is true"); // 
 		}	
 		else {
