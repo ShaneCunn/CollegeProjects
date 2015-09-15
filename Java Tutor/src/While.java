fa62aee3661121f7fb00344f1923c7a6c4fe9f87
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 15:21:38 +0100

    do while loop

--- Java Tutor/src/While.java
+++ Java Tutor/src/While.java
@@ -4,14 +4,24 @@ public class While {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int  a = 10;
+		int  a = 0;
 		
-		while (a >= 1) // while a is less than one is loops through
+		while (a <= 10) // while a is less than one is loops through
 		{
 			System.out.println(a);
-			a--; // adds +1 to the A counter
+			a++; // adds +1 to the A counter
 		}
-
+		
+		System.out.println("-------------------------------------");
+		int b = 0;
+		
+		do {
+			
+			System.out.println(b);
+			b++; // adds +1 to the B counter			
+			
+			
+		} while (b <= 10);
 	}
 
 }
