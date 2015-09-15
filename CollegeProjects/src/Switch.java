096948f248a157fc0160dd4bbd5be8575c68e05f
Shane Cunningham <mayhem2277@gmail.com>
2015-09-15 13:55:23 +0100

    Switch statement

---
+++ CollegeProjects/src/Switch.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+/**
+ * @author shane
+ *
+ */
+public class Switch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// can be a byte , short, int , or char
+		int score = 90;
+		switch (score )
+		{
+		case 90 : 
+			System.out.println("Very Good");
+			break; // break out of the switch statement
+		case 60 :
+			System.out.println("Good");
+		
+			break; // break out of the switch statement 
+		case 30 :
+			System.out.println("not Good");
+			break; // break out of the switch statement
+			
+			
+			
+		}
+		
+	}
+
+}
