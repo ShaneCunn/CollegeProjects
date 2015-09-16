f04dde805c7071f9a65cdecd66c16e3e77b5c1cc
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 18:14:31 +0100

    add comments to the bike test class

--- CollegeProjects/src/BikeTester.java
+++ CollegeProjects/src/BikeTester.java
@@ -5,12 +5,13 @@ public class BikeTester {
 		Bike  bike1, bike2;
 		String  owner1, owner2;
 		
-		bike1 = new Bike();
-		bike1.setOwnerName("Shane Cunningham");
+		bike1 = new Bike(); // declares and creates a bike object
+		bike1.setOwnerName("Shane Cunningham"); // assign a value to bike1
 		
-		bike2 = new Bike();
-		bike2.setOwnerName("John McQuillian");
+		bike2 = new Bike(); //Declare and Create object 
+		bike2.setOwnerName("John McQuillian"); // assign a value to bike2
 		
+		//outs info on the 2 bikes
 		owner1 = bike1.getOwnerName();
 		owner2 = bike2.getOwnerName();
 		
