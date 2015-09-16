c1a0b2039c57f2a88e0abf0ac62ad2e480754764
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 17:48:30 +0100

    Bike class

---
+++ CollegeProjects/src/Bike.java
@@ -0,0 +1,28 @@
+
+public class Bike {
+
+	private String ownerName; // data member
+	
+	//constructor
+	
+	public Bike() 
+	{
+		ownerName =  "Unassigned";
+				
+	}
+	public String getOwnerName(){ 
+	
+	return ownerName;
+	}
+	
+	public void setOwnerName(String name)
+	{
+	
+		ownerName = name;
+		
+		
+	}
+	
+	
+
+}
