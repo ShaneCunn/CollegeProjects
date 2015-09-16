12b7ee9e5891d6526eb64fdbeedc36c86aa9a214
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 18:51:59 +0100

    take in a number from the user and displays the secret number and ther guess

--- CollegeProjects/src/CollegePackage/GuessingNumberGame.java
+++ CollegeProjects/src/CollegePackage/GuessingNumberGame.java
@@ -1,5 +1,7 @@
 package CollegePackage;
 
+import java.util.Scanner;
+
 public class GuessingNumberGame {
 
 	public static void main(String[] args) {
@@ -8,6 +10,14 @@ public class GuessingNumberGame {
 		int secretNumber;
 		secretNumber = (int) (Math.random()*10);
 		System.out.println("Secert number is " + secretNumber);
+		
+		Scanner keyboard = new Scanner(System.in);
+		int deducted;
+		System.out.println("enter a guess: ");
+		deducted = keyboard.nextInt();
+		System.out.println("your guess is " + deducted);
+		
+		
 		
 		
 	}
