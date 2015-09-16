3cfea9284c4aeb53bab33708b8a9df42194f0141
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 18:16:27 +0100

    added comments to the bike class

--- CollegeProjects/src/Bike.java
+++ CollegeProjects/src/Bike.java
@@ -3,26 +3,24 @@ public class Bike {
 
 	private String ownerName; // data member
 	
-	//constructor
-	
+	//constructor	
 	public Bike() 
 	{
 		ownerName =  "Unassigned";
 				
 	}
+	
+	// returns the name of this bike's owner
 	public String getOwnerName(){ 
 	
-	return ownerName;
+		return ownerName;
 	}
 	
+	//assigns the name of this bike owner
 	public void setOwnerName(String name)
-	{
-	
-		ownerName = name;
+	{	
+		ownerName = name;	
 		
-		
-	}
-	
-	
+	}	
 
 }
