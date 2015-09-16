4ed2d9b81b6c3ff38cf27067a19d43096137dd50
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 18:59:48 +0100

    added a do and while

--- CollegeProjects/src/CollegePackage/GuessingNumberGame.java
+++ CollegeProjects/src/CollegePackage/GuessingNumberGame.java
@@ -11,12 +11,25 @@ public class GuessingNumberGame {
 		secretNumber = (int) (Math.random()*10);
 		System.out.println("Secert number is " + secretNumber);
 		
-		Scanner keyboard = new Scanner(System.in);
-		int deducted;
-		System.out.println("enter a guess: ");
-		deducted = keyboard.nextInt();
-		System.out.println("your guess is " + deducted);
 		
+		
+		Scanner keyboard = new Scanner(System.in);
+		int guess;
+		
+		do {
+		System.out.println("Enter a guess: ");
+		guess = keyboard.nextInt();
+		System.out.println("Your guess is " + guess);
+		
+		
+		
+		if (guess == secretNumber)
+			System.out.println("Your guess is correct, well done");
+		else if (guess < secretNumber)
+			System.out.println("Your guess is smaller than the secret number.");
+		else if (guess > secretNumber)
+			System.out.println(" Your number is larger than the secret number.");
+		} while (guess != secretNumber);
 		
 		
 		
