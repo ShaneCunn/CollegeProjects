cb415590ad925dbc54262d7922dd911d0e47486a
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 18:31:18 +0100

    fixed the scanner in integer with a try

--- CollegeProjects/src/CollegePackage/interger.java
+++ CollegeProjects/src/CollegePackage/interger.java
@@ -5,13 +5,16 @@ public class interger {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner single = new Scanner(System.in);
 		
-		int lads, girls, people;
-			lads = 6;
-			girls= 3;
-			people = lads + girls;
+		// added a try to close the scanner
+		try (Scanner single = new Scanner(System.in)) 
+			{
+			int lads, girls, people;
+				lads = 6;
+				girls= 3;
+				people = lads + girls;
 				System.out.println("The is Total: " + people);
+		}
 
 	}
 
