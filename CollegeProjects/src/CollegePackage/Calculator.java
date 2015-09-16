0500f00f462e750abc17b9f6a179212d0f25663f
Shane Cunningham <mayhem2277@gmail.com>
2015-09-16 19:35:34 +0100

    fixed calculator and limted the attempts on the guess number game to 7

--- CollegeProjects/src/CollegePackage/Calculator.java
+++ CollegeProjects/src/CollegePackage/Calculator.java
@@ -1,19 +1,20 @@
 package CollegePackage;
 import  java.util.Scanner; //import scanner that read in system in
 
-public class Calculator {
+public class Calculator 
+{
 	public static void main(String[] args) {
-		Scanner Cal = new Scanner(System.in);
+	try	(Scanner Cal = new Scanner(System.in))
+			{
+			double fnum, snum, answer; // 3 double varible
 		
-		double fnum, snum, answer; // 3 double varible
-		
-		System.out.println("Enter the first number:");
-		fnum = Cal.nextDouble(); //stores first number			
-		System.out.println("Enter the second number:");
-		snum = Cal.nextDouble(); // store the second number		
-		answer = fnum + snum; // add toget the first and second numbers
-		System.out.println(answer); // outputs the answer variable to screen
-			
+			System.out.println("Enter the first number:");
+			fnum = Cal.nextDouble(); //stores first number			
+			System.out.println("Enter the second number:");
+			snum = Cal.nextDouble(); // store the second number		
+			answer = fnum + snum; // add toget the first and second numbers
+			System.out.println(answer); // outputs the answer variable to screen
+			}	
 		
 	}
 }
--- CollegeProjects/src/CollegePackage/GuessingNumberGame.java
+++ CollegeProjects/src/CollegePackage/GuessingNumberGame.java
@@ -6,38 +6,48 @@ public class GuessingNumberGame {
 
 	public static void main(String[] args) {
 		
-		
+		// Declares Variables
 		int secretNumber;
 		secretNumber = (int) (Math.random()*1000 + 1);
-		//System.out.println("Secert number is " + secretNumber);
+		//System.out.println("Secret number is " + secretNumber);
 		
 		
-		
+		//Declares and creates a scanner object
 		try (Scanner keyboard = new Scanner(System.in))
 		{
 		
 		
 		int guess;
+		int attempts = 0;
 		
-		do {
-		System.out.println("Enter a guess (1-1000): ");
-		guess = keyboard.nextInt();
-		System.out.println("Your guess is " + guess);
-		
-		
-		
-		if (guess == secretNumber)
-			System.out.println("Your guess is correct, well done");
-		else if (guess < secretNumber)
-			System.out.println("Your guess is smaller than the secret number.");
-		else if (guess > secretNumber)
-			System.out.println("Your number is larger than the secret number.");
-		} while (guess != secretNumber);
-		
-		
-		}
-		
-		
+			do {
+				// get input from user
+				System.out.println("Enter a guess (1-1000): ");
+				guess = keyboard.nextInt();
+				System.out.println("Your guess is " + guess);				
+				
+				
+				if (guess == secretNumber)
+					System.out.println("Your guess is correct, well done");
+				else if (guess < secretNumber)
+					System.out.println("Your guess is smaller than the secret number.");
+				else if (guess > secretNumber)
+					System.out.println("Your number is larger than the secret number.");
+				
+				
+				// keeps going till the right number is guessed
+				attempts++;
+				} while (guess != secretNumber && attempts < 7);
+			
+			if(guess == secretNumber){
+				System.out.println("Your guess is correct, well done");				
+				
+			}
+			else{
+				System.out.println("You tried incorrectly 7 times!!!");
+				
+			}
+		}	
 		
 		
 	}
