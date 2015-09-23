097f531b29cf0131cf7af3c12031bf6a56f39b6d
Shane Cunningham <mayhem2277@gmail.com>
2015-09-23 22:49:27 +0100

    add and multiply together all 6 variable and prints out to screen

--- CollegeAssignments/src/Assignments/ConcertTickets.java
+++ CollegeAssignments/src/Assignments/ConcertTickets.java
@@ -3,11 +3,34 @@ package Assignments;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-
 public class ConcertTickets {
 
 	public static void main(String[] args) {
-		
+
+		try (Scanner Cal = new Scanner(System.in)) {
+			double fnum, snum, thirdnum, fourthNum, fithNum, sixNum,  answer; 
+			
+			// 3 double variable
+
+			System.out.println("Enter number of A seats sold:");
+			fnum = Cal.nextDouble(); // stores first number
+			System.out.println("Enter price of A tickets:");
+			snum = Cal.nextDouble(); // store the second number
+			
+			System.out.println("Enter number of B seats sold:");
+			thirdnum = Cal.nextDouble(); // stores  number
+			System.out.println("Enter price of B tickets:");
+			fourthNum = Cal.nextDouble(); // store the second number
+			
+			System.out.println("Enter number of C seats sold:");
+			fithNum = Cal.nextDouble(); // stores first number
+			System.out.println("Enter price of C tickets:");
+			sixNum = Cal.nextDouble(); // store the second number
+			
+			
+			answer = ((fnum * snum) + (thirdnum * fourthNum) + (fithNum * sixNum) ); // add together the first and second numbers
+			System.out.println(answer); // outputs the answer variable to screen
+		}
 
 	}
 
